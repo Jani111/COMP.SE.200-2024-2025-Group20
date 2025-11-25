3c380ba44938cc1eb54b01a721fc82cdcacb8f97
Joni Salonen <joni.salonen@tuni.fi>
2025-11-25 10:34:45 +0200

    implemented tests for functions ceil,map and words

---
+++ test/ceil_test.js
@@ -0,0 +1,30 @@
+import { expect } from "chai"
+import ceil from "../src/ceil.js"
+
+describe("ceilTests", () => {
+
+    it("should round 4.006 up to 5", () => {
+        expect(ceil(4.006)).to.equal(5)
+    })
+
+    it("should round 6.004 up to 6.01 with precision 2", () => {
+        expect(ceil(6.004, 2)).to.equal(6.01)
+    })
+
+    it("should round 6040 up to 6100 with precision -2", () => {
+        expect(ceil(6040, -2)).to.equal(6100)
+    })
+
+    it("should round -4.006 up to -4", () => {
+        expect(ceil(-4.006)).to.equal(-4)
+    })
+
+    it("should return NaN for NaN input", () => {
+        expect(isNaN(ceil(NaN))).to.equal(true)
+    })
+
+    it("should return NaN for undefined input", () => {
+        expect(isNaN(ceil(undefined))).to.equal(true)
+    })
+
+})
---
+++ test/words_test.js
@@ -0,0 +1,31 @@
+import { expect } from "chai"
+import words from "../src/words.js"
+
+describe("wordsTests", () => {
+
+    it("should split 'fred, barney, & pebbles' into ASCII words", () => {
+        expect(words("fred, barney, & pebbles")).to.deep.equal(["fred", "barney", "pebbles"])
+    })
+
+    it("should split using custom regex pattern", () => {
+        expect(words("fred, barney, & pebbles", /[^, ]+/g))
+            .to.deep.equal(["fred", "barney", "&", "pebbles"])
+    })
+
+    it("should split '123abcDEF' into separate alphanumeric parts", () => {
+        expect(words("123abcDEF")).to.deep.equal(["123", "abc", "DEF"])
+    })
+
+    it("should detect unicode words like 你好世界", () => {
+        expect(words("你好世界")).to.deep.equal(["你好世界"])
+    })
+
+    it("should return [] for a string with only spaces", () => {
+        expect(words("   ")).to.deep.equal([])
+    })
+
+    it("should throw TypeError for undefined input", () => {
+        expect(() => words(undefined)).to.throw(TypeError)
+    })
+
+})
